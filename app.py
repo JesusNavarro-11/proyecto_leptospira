205d7a6fd7063abbb3e90a21e7ee2ed35f7d8340
JesusNavarro-11 <jesusramirez1122a@gmail.com>
2024-12-30 15:39:32 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+# Streamlit app principal
+import streamlit as st
+
+# Título de la app
+st.title("Sistema de Identificación de Leptospira Interrogans")
+
+# Punto de entrada
+if __name__ == "__main__":
+    st.write("Interfaz en construcción...")
