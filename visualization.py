27a421001049327a1d427e66d626981f99a899e0
JesusNavarro-11 <jesusramirez1122a@gmail.com>
2025-01-17 09:01:59 -0600

    Delete visualization.py

--- visualization.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import matplotlib.pyplot as plt
-
-def show_results():
-    metrics = {"Precisión": 0.95, "Sensibilidad": 0.92, "Especificidad": 0.88}
-    fig, ax = plt.subplots()
-    ax.bar(metrics.keys(), metrics.values())
-    st.pyplot(fig)
