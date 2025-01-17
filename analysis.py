2c318bc75aeed04ebbbaf36a6926c8d06bbd365c
JesusNavarro-11 <jesusramirez1122a@gmail.com>
2025-01-17 09:02:13 -0600

    Delete analysis.py

--- analysis.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def analyze_samples():
-    st.write("Iniciando análisis...")
-    if st.button("Analizar"):
-        st.success("Leptospira identificada. Precisión: 95%.")
