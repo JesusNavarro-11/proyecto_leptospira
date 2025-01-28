f23c944bb97b793d50dfb72627f87f1207ab6989
JesusNavarro-11 <jesusramirez1122a@gmail.com>
2025-01-27 23:41:50 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,12 @@ echo "🔧 Instalando PyTorch y dependencias relacionadas..."
 pip install --no-cache-dir torch==2.0.1+cpu torchvision==0.15.2+cpu torchaudio==2.0.1+cpu -f https://download.pytorch.org/whl/cpu
 
 echo "🔧 Instalando dependencias generales..."
-pip install --no-cache-dir gdown opencv-python==4.8.0.76 Pillow numpy streamlit fpdf
+pip install --no-cache-dir gdown Pillow numpy streamlit fpdf
+
+echo "🔧 Instalando OpenCV..."
+pip uninstall -y opencv-python opencv-python-headless  # Elimina versiones previas si existen
+pip install --no-cache-dir opencv-python-headless==4.8.0.76
+pip install --no-cache-dir opencv-python==4.8.0.76
 
 echo "🔧 Instalando Detectron2 desde GitHub (versión compatible con PyTorch 2.0.1)..."
 pip install --no-cache-dir git+https://github.com/facebookresearch/detectron2.git@v0.6
